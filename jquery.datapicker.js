bdb4bb6968b9a2d598d7c621a79a29976b793fb7
leaven <dlwelllee@gmail.com>
2015-01-22 14:52:45 +0800

    add js file

---
+++ jquery.datapicker.js
@@ -0,0 +1,41 @@
+/**
+ * @des 通过在dom元素上加一层data-*过滤，在触发onclick事件冒泡的时候进行拦截，并触发我们自定义的callback函数
+ */
+(function($, window, undefined) {
+	$.fn.dataPicker =  function(options) {
+		
+		var datapicker = new DataPicker(this, options);
+		return domsel.init();
+	}
+
+
+	var DataPicker = function(el, options) {
+		this.$element = el;
+		this.defaults  = {
+			attr : "static",
+			event : "click",
+			callback : function(){}
+		}
+		this.settings = $.extend({}, this.defaults, options);
+	}
+	DataPicker.prototype = {
+		init : function() {
+			var self = this;
+			this.$element.bind(self.settings.event, function(event) {
+				var $se = $(event.target);
+				if(typeof $se.data("click-cache") !== 'undefined')
+					return ;
+				//增加cache，防止重复点击
+				$se.data("click-cache", true);
+				var $loginDom = $se.data(self.settings.attr);
+				if(typeof $loginDom === 'undefined') {
+					$loginDom = $se.parents("[data-"+self.settings.attr+"]")
+				}
+				if(typeof $loginDom !== "undefined" && $loginDom.length != 0) {
+					self.settings.callback();
+					$se.removeData("click-cache");
+				}
+			});
+		}
+	}
+}($,window))
